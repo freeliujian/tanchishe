ed70b1644ffdcfaf3049b00545b80d8ee60f4b8a
littleforest <670177821@qq.com>
2022-06-13 15:36:57 +0800

    feat: 新增游戏开始封面和碰撞事件

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import {
-  AbstractRenderer,
   Application,
-  autoDetectRenderer,
   Container,
   Sprite,
   Loader,
   Graphics,
+  Text,
+  TextStyle
 } from "pixi.js";
 import { Shetou, ShetouProps } from "./shetou";
 import { keyboard } from "./keyboard";
@@ -24,15 +24,13 @@ interface SpeedProps {
 }
 
 class Core {
-  element: string;
+  element: Element;
   width: string | number;
   height: string | number;
-  //renderer!: AbstractRenderer | null
   renderer!: any;
-
-  container!: any;
   loader!: any;
   isLoader?: boolean;
+  isVisible: boolean;
   speed: SpeedProps;
   state: any;
   shetou: any;
@@ -42,27 +40,33 @@ class Core {
 
   constructor(option: CoreProps) {
     const { element, width, height, isLoader = false } = option;
-
-    this.element = element;
+    this.element = document.querySelector(`#${element}`); 
     this.width = width;
     this.height = height;
     this.isLoader = isLoader;
     this.sheshen = [0, 0, 0];
+    this.isVisible = true;
+    this.renderer = null;
     this.init();
+    
   }
 
   init() {
     console.log("init");
-    this.renderer = null;
-    this.container = null;
     this.apple = null;
     this.wallArr = null;
     this.speed = {
       speedY: 0,
       speedX: 0,
     };
+    this.renderer = new Application({
+      width: this.width as number,
+      height: this.height as number,
+      backgroundColor: 0xffffff,
+    });
     this.createRender();
   }
+
 
   loaderImg(option: {
     imgUrl: string;
@@ -74,60 +78,101 @@ class Core {
   }
 
   createRender() {
-    const box = document.querySelector(`#${this.element}`);
-    //this.renderer = autoDetectRenderer({width:this.width as number , height:this.height as number ,backgroundColor:0xffffff})
-    this.renderer = new Application({
-      width: this.width as number,
-      height: this.height as number,
-      backgroundColor: 0xffffff,
-    });
+    this.renderer.stage.removeChildren();
+    this.element?.appendChild(this.renderer.view);
+    console.log(this.renderer.stage)
+    if(this.isVisible){
+      this.createWalls();
+      this.createApple();
+      this.createShetou();
 
-    box?.appendChild(this.renderer.view);
-    this.createWalls();
-    this.createApple();
-    this.createShetou();
+    }else {
+
+      this.startPage();
+    }
   }
+
+  startPage(){
+
+    const {width,height} = this;
+    const startContainer = new Container();
+    const graphics = new Graphics();
+    graphics.beginFill(0x000000);
+    graphics.drawRect(0,0,width as number,height as number);
+    graphics.endFill();
+    startContainer.addChild(graphics);
+    const style = new TextStyle({
+      fontFamily: "Arial",
+      fontSize: 36,
+      fill: "white",
+      stroke: '#ff3300',
+      strokeThickness: 4,
+      dropShadow: true,
+      dropShadowColor: "#cccccc",
+      dropShadowBlur: 4,
+      dropShadowAngle: Math.PI / 6,
+      dropShadowDistance: 6,
+    }); 
+    const message = new Text('贪吃蛇（乞丐版）',style);
+    message.position.set(width as number/3,height as number/4)
+    startContainer.addChild(message);
+    const border = new Graphics();
+    border.beginFill(0xcccccc);
+    border.lineStyle({ color: 0xffffff, width: 4, alignment: 0 });
+    border.drawRect(0,0,120,60);
+    border.endFill();
+    border.position.set(this.width as number/2.5,height as number/2)
+    startContainer.addChild(border);
+    const button = new Text('游戏开始');
+    console.log(border)
+    border.interactive = true;
+    border.on('pointerdown', e => {
+     
+      this.isVisible = true;
+      this.createRender();
+    });
+    button.position.set(this.width as number/2.45,height as number/1.9)
+    startContainer.addChild(button)
+
+    this.renderer.stage.addChild(startContainer);
+
+  };
 
   createWalls() {
     console.log("created Wall");
     const container = new Container();
     this.renderer.stage.addChild(container);
-    //LeftWall
-    const LeftWall = new Graphics();
-    LeftWall.beginFill(0xde3249);
-    LeftWall.drawRect(0, 0, 10, this.height as number);
-    LeftWall.endFill();
-    container.addChild(LeftWall);
+    //eftWall
+    const leftWall = new Graphics();
+    leftWall.beginFill(0xde3249);
+    leftWall.drawRect(0, 0, 10, this.height as number);
+    leftWall.position.set(0, 0);
+    leftWall.endFill();
+    container.addChild(leftWall);
     //rightWall
     const rightWall = new Graphics();
     rightWall.beginFill(0xde3249);
-    rightWall.drawRect(
-      (this.width as number) - 10,
-      0,
-      10,
-      this.height as number
-    );
+    rightWall.drawRect(0, 0, 10, this.height as number);
     rightWall.endFill();
+    rightWall.position.set((this.width as number) - 10, 0);
     container.addChild(rightWall);
     //topWall
     const topWall = new Graphics();
     topWall.beginFill(0xde3249);
     topWall.drawRect(0, 0, this.width as number, 10 as number);
+    topWall.position.set(0, 0);
     topWall.endFill();
     container.addChild(topWall);
     //bottomWall
     const bottomWall = new Graphics();
     bottomWall.beginFill(0xde3249);
-    bottomWall.drawRect(
-      0,
-      (this.height as number) - 10,
-      this.width as number,
-      this.height as number
-    );
+    bottomWall.drawRect(0, 0, this.width as number, 10);
+    topWall.position.set(0, (this.height as number) - 10);
     bottomWall.endFill();
 
     this.wallArr = {
-      LeftWall: LeftWall,
+      wall: container,
+      leftWall: leftWall,
       rightWall: rightWall,
       topWall: topWall,
       bottomWall: bottomWall,
@@ -142,7 +187,7 @@ class Core {
     this.apple = Sprite.from(Apple);
     this.apple.width = 25;
     this.apple.height = 30;
-    this.apple.position.set(10, 600);
+    this.apple.position.set(10, 200);
     container.addChild(this.apple);
   }
 
@@ -180,27 +225,32 @@ class Core {
   }
 
   play(_delta: any) {
-    const {speed,shetou,apple,wallArr,hitTestRectangle} = this
+    const { speed, shetou, apple, wallArr, hitTestRectangle } = this;
     const { speedX, speedY } = speed;
-    const {LeftWall,rightWall,topWall,bottomWall} = wallArr
+    const { leftWall, rightWall, topWall, bottomWall, wall } = wallArr;
     shetou.x += speedX;
     shetou.y += speedY;
 
     if (hitTestRectangle(shetou, apple)) {
       console.log("你碰到了苹果");
+
     }
-    if (hitTestRectangle(shetou, LeftWall)) {
-      console.log("左边墙壁");
+    if (hitTestRectangle(shetou, leftWall)) {
+      this.over();
     }
     if (hitTestRectangle(shetou, rightWall)) {
-      console.log("右边墙壁");
+      this.over();
     }
-    // if (hitTestRectangle(shetou, topWall)) {
-    //   console.log("上面的墙壁");
-    // }
-    // if (hitTestRectangle(shetou, bottomWall)) {
-    //   console.log("下面的墙壁");
-    // }
+    if (hitTestRectangle(shetou, topWall)) {
+      this.over();
+    }
+    if (hitTestRectangle(shetou, bottomWall)) {
+      this.over();
+    }
+  }
+
+  over() {
+    console.log('game over');
   }
 
   move() {
@@ -217,11 +267,11 @@ class Core {
       this.speed = action;
     };
 
-    left.release = () => {
-      if (!right.isDown && this.speed.speedY === 0) {
-        this.speed.speedX = 0;
-      }
-    };
+    // left.release = () => {
+    //   if (!right.isDown && this.speed.speedY === 0) {
+    //     this.speed.speedX = 0;
+    //   }
+    // };
 
     up.press = () => {
       const action = {
@@ -231,11 +281,11 @@ class Core {
       this.speed = action;
     };
 
-    up.release = () => {
-      if (!down.isDown && this.speed.speedX === 0) {
-        this.speed.speedY = 0;
-      }
-    };
+    // up.release = () => {
+    //   if (!down.isDown && this.speed.speedX === 0) {
+    //     this.speed.speedY = 0;
+    //   }
+    // };
 
     //Right
     right.press = () => {
@@ -246,11 +296,11 @@ class Core {
       this.speed = action;
     };
 
-    right.release = () => {
-      if (!left.isDown && this.speed.speedY === 0) {
-        this.speed.speedX = 0;
-      }
-    };
+    // right.release = () => {
+    //   if (!left.isDown && this.speed.speedY === 0) {
+    //     this.speed.speedX = 0;
+    //   }
+    // };
 
     //Down
     down.press = () => {
@@ -260,18 +310,17 @@ class Core {
       };
       this.speed = action;
     };
-    down.release = () => {
-      if (!up.isDown && this.speed.speedX === 0) {
-        this.speed.speedY = 0;
-      }
-    };
+    // down.release = () => {
+    //   if (!up.isDown && this.speed.speedX === 0) {
+    //     this.speed.speedY = 0;
+    //   }
+    // };
   }
 
   hitTestRectangle(r1: any, r2: any) {
     let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
 
     hit = false;
-
     r1.centerX = r1.x + r1.width / 2;
     r1.centerY = r1.y + r1.height / 2;
     r2.centerX = r2.x + r2.width / 2;
@@ -304,6 +353,6 @@ class Core {
 
 const tanchishe = new Core({
   element: "container",
-  width: "1000",
-  height: "1200",
+  width: "800",
+  height: "600",
 });
